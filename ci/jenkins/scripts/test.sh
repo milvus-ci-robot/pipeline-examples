92a30ccf16c2dfa55cb077fa6b65daddf9d8841d
quicksilver <zhifeng.zhang@zilliz.com>
2019-11-26 21:24:29 +0800

    update test.sh

--- ci/jenkins/scripts/test.sh
+++ ci/jenkins/scripts/test.sh
@@ -6,4 +6,6 @@ git log --decorate | head -n 1 | sed 's/.*(\(.*\))/\1/' | sed 's/.*, //' | sed '
 
 if [[ ${BRANCH_NAME} =~ "PR-" && ! -n ${CHANGE_BRANCH} ]];then
     echo "BRANCH_NAME: ${BRANCH_NAME} CHANGE_BRANCH: ${CHANGE_BRANCH}"
+else
+    echo "BRANCH_NAME: ${BRANCH_NAME}"
 fi
