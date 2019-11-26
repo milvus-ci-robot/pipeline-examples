ad103690121314751bed7b3f00ae1a5438c7cdf3
quicksilver <zhifeng.zhang@zilliz.com>
2019-11-26 21:22:52 +0800

    update test.sh

--- ci/jenkins/scripts/test.sh
+++ ci/jenkins/scripts/test.sh
@@ -3,3 +3,7 @@
 git log --decorate | head -n 1 | sed 's/.*(\(.*\))/\1/'
 git log --decorate | head -n 1 | sed 's/.*(\(.*\))/\1/' | sed 's/.*, //'
 git log --decorate | head -n 1 | sed 's/.*(\(.*\))/\1/' | sed 's/.*, //' | sed 's=[a-zA-Z]*\/==g'
+
+if [[ ${BRANCH_NAME} =~ "PR-" && ! -n ${CHANGE_BRANCH} ]];then
+    echo "BRANCH_NAME: ${BRANCH_NAME} CHANGE_BRANCH: ${CHANGE_BRANCH}"
+fi
